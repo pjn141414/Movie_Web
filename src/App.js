8e357aa12c453507d9c1a9d4d6b8e145f915b6f4
pjn141414 <jina071049@gmail.com>
2021-05-24 11:21:29 +0900

    Add: rating 추가

--- src/App.js
+++ src/App.js
@@ -7,10 +7,11 @@ function Movie() {
   )
 }
 
-function Food({ name, picture }) {
+function Food({ name, picture, rating }) {
   return (
     <div>
       <h2>I like {name}</h2>
+      <h4>{rating}/5.0</h4>
       <img src={picture} alt={name} />
     </div>
   )
@@ -22,29 +23,33 @@ Food.propTypes = {
 
 function renderFood(dish) {
   console.log(dish);
-  return <Food name={dish.name} picture={dish.image} />
+  return <Food name={dish.name} picture={dish.image} rating={dish.rating} />
 }
 
 const foodILike = [
   {
     idx: 1,
     name: "Nacho",
-    image: "https://img.insight.co.kr/static/2017/10/21/700/f8e31isod6z42978jqtw.jpg"
+    image: "https://img.insight.co.kr/static/2017/10/21/700/f8e31isod6z42978jqtw.jpg",
+    rating: 4
   },
   {
     idx: 2,
     name: "Ramen",
-    image: "https://imgcp.aacdn.jp/img-a/1200/auto/global-aaj-front/article/2018/06/5b321f26e22f9_5b321d8b3819e_261033538.jpeg"
+    image: "https://imgcp.aacdn.jp/img-a/1200/auto/global-aaj-front/article/2018/06/5b321f26e22f9_5b321d8b3819e_261033538.jpeg",
+    rating: 3
   },
   {
     idx: 3,
     name: "Kimchi",
-    image: "https://newsimg.hankookilbo.com/cms/articlerelease/2020/04/23/202004231500779914_1.jpg"
+    image: "https://newsimg.hankookilbo.com/cms/articlerelease/2020/04/23/202004231500779914_1.jpg",
+    rating: 5
   },
   {
     idx: 4,
     name: "hi",
-    image: "http://www.dentalarirang.com/news/photo/201708/18294_24504_3851.jpg"
+    image: "http://www.dentalarirang.com/news/photo/201708/18294_24504_3851.jpg",
+    rating: 1
   }
 ];
 
