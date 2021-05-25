6954684f005a65b1f95c88cfe24cc4e0554aafcb
pjn141414 <jina071049@gmail.com>
2021-05-25 15:26:54 +0900

    Add: Movie.js 생성까지

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
 import reactDom from "react-dom";
+import axios from "axios";
+import Movie from "./Movie";
 
 class App extends React.Component {
   state = {
@@ -8,16 +10,35 @@ class App extends React.Component {
     movies: []
   }
 
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?  sort_by=rating");
+    console.log(movies);
+    this.setState({ movies, isLoading: false });
+  }
+
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 6000);
+    this.getMovies();
   }
 
   render() {
-    const { isLoading } = this.state;
+    const { isLoading, movies } = this.state;
     return (
-      <div>{this.state.isLoading ? "Loading" : "We are ready"}</div>
+      <div>
+        {this.state.isLoading ? "Loading..." : this.state.movies.map(movie => {
+          console.log(movie);
+          return (<Movie
+            id={movies.id}
+            year={movies.year}
+            title={movies.title}
+            summary={movies.summary}
+            poster={movies.medium_cover_image} />
+          );
+        })}
+      </div>
     )
   };
 }
