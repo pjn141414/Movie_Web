b33ec847575502b24f45a94fd6257e091ca6e367
pjn141414 <jina071049@gmail.com>
2021-06-01 19:27:11 +0900

    Add: css 적용

--- src/Movie.js
+++ src/Movie.js
@@ -5,11 +5,15 @@ import "./Movie.css";
 function Movie({ year, title, summary, poster, genres }) {
   return (
     <div className="movie">
+      <img src={poster} alt={title} title={title} />
       <div className="movie__data">
         <h3 className="movie__title">{title}</h3>
         <h5 className="movie__year">{year}</h5>
-        <img src={poster} alt={title} title={title} />
-        <p className="movie__summary">{summary}</p>
+        <ul className="movie__genres">{genres.map((genre, index) =>
+          <li key={index} className="genres_genre">{genre}</li>
+        )}
+        </ul>
+        <p className="movie__summary">{summary.slice(0, 180)}...</p>
       </div>
     </div>
   );
